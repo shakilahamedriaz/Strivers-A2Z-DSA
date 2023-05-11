b77bf41f1f465692dcdcccc6a2ab52894f356bcc
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-05-11 15:28:28 +0600

    done

---
+++ Step1-Learn Basic/switch 1.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+int day;
+cin>>day;
+
+  switch(day){
+
+  case 1:
+
+    cout<< "Monday";
+    break;
+
+  case 2:
+
+      cout<< "Tuesday";
+      break;
+
+
+  case 3:
+
+      cout<< "Wendnesday";
+
+  case 4:
+
+      cout<< "Thursday";
+      break;
+
+  case 5:
+
+      cout<< "Friday";
+      break;
+
+  case 6:
+
+      cout<< "Saturday";
+      break;
+
+   case 7:
+
+    cout<< "Sunday";
+    break;
+
+   default:
+
+    cout<< "INvalid";
+
+
+  }
+
+
+
+
+
+return 0;
+}
