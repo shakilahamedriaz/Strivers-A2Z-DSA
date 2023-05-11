fec706cceff9e9fa1a50b7d01f013d2739fc470e
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-05-11 14:53:47 +0600

    Done

---
+++ Step1-Learn Basic/char 1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+
+{
+
+    char ch;
+    cin>>ch;
+    cout<< ch<<endl;
+
+    char h= 'g';
+    cout<<h<<endl;
+
+
+
+
+    return 0;
+}
---
+++ Step1-Learn Basic/variable.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s1;
+    string s2;
+
+    cin>>s1>>s2;
+    cout<<s1<< " "<<s2<<endl;
+
+    return 0;
+}
