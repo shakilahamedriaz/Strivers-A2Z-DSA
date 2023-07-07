8031aa9f861c4103515f0f885368934da9e238b6
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-07-08 03:16:40 +0600

    done

---
+++ Step1-Learn Basic/Step 1.5 Learn Basic Recursion/printing numbers 0 to 3.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int cnt = 0;
+void print()
+{
+    
+    // Base conditon:
+    if(cnt == 10) return;
+    cout<<cnt<<endl;
+
+    //count Increment
+    cnt++;
+    print();
+}
+
+int main()
+{
+    print();
+    return 0;
+}
