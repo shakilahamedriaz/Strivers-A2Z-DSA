2967db69ab4714fb0f9958467e284604afa2ca42
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-07-31 23:25:50 +0600

    dsa

---
+++ step2-Learn Important Sorting Techniques/step 2.1 sorting-I/1 selection sort.cpp
@@ -0,0 +1,21 @@
+// algorithm:
+
+//select minimum
+//swap it with i(sequentially)
+
+/*
+
+ figure out the minimum, then swap the array 1st elements
+ then agian find the min and swap with the arrays 2nd element
+ continue this process;
+
+for(int i=0; i<n-2; i++)
+{
+    for(int j=1; j<=n-1; j++)
+    {
+        
+    }
+} 
+
+
+*/
