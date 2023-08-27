a5bc48c71b4d80d06dcb79eefae4aa7fd6ea651f
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-08-28 00:51:53 +0600

    Done

--- step3-Solving Problems on Arrays/3.1 -Easy/7 Remove Duplicate Bruteforce.cpp
+++ step3-Solving Problems on Arrays/3.1 -Easy/7 Remove Duplicate Bruteforce.cpp
@@ -1,39 +1,42 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int removeDuplicate(int arr[], int n)
 {
     set<int> st;
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
         st.insert(arr[i]);
     }
 
-    int k= st.size();
-    int j=0;
-
-    for(int x: st)
+    int j = 0;
+    for (int x : st)
     {
-        arr[j++]=x;
+        arr[j++] = x;
     }
 
-    return k;
+    return j; // Return the size of the modified array
 }
-
 
 int main()
 {
-     int arr[]= {1,1,1,2,2,2,3,3,3,4,4,4,5,5,5};
-     int n= sizeof(arr)/sizeof(arr[0]);
+    int n;
+    cin >> n;
 
-     int k= removeDuplicate(arr, n);
-     
-     cout<< "Total unique array  : "<< k<<endl; // "Total unique array  : 5
-     cout<< "Array after removing duplicate elements: ";
-     for(int i=0; i<k; i++)
-     {
-        cout<< arr[i]<< " ";
-     }
+    int arr[n]; // Declare an array of size 'n'
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
 
-   return 0;
+    int k = removeDuplicate(arr, n);
+
+    cout << "Total unique array  : " << k << endl;
+    cout << "Array after removing duplicate elements: ";
+    for (int i = 0; i < k; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
 }
