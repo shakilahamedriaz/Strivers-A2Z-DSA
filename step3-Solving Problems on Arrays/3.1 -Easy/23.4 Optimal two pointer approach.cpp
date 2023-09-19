940f62e19605196ea4fe40cce8880542694aa279
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-09-19 22:45:59 +0600

    Done

---
+++ step3-Solving Problems on Arrays/3.1 -Easy/23.4 Optimal two pointer approach.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int getLongestSubarray(vector<int>& a,int n, long long k) {
+      
+      int i=0, j=0, maxLen=INT_MIN;
+      long long sum=0;
+      while(j<n)
+      {
+        sum+=a[j];
+        if(sum==k)
+        {
+            maxLen=max(maxLen,j-i+1);
+            j++;
+        }
+        else if(sum<k)
+        {
+            j++;
+        }
+        else if(sum>k)
+        {
+            while(sum>k)
+            {
+                sum-=a[i];
+                i++;
+            }
+            if(sum==k)
+            {
+                maxLen=max(maxLen,j-i+1);
+            }
+            j++;
+        }
+      }
+
+      return maxLen;
+}
+
+int main()
+{
+    vector<int> a = {1,2,3,1,1,1,1,4,2,2};           //{2, 3, 5, 1, 9};
+    int n=a.size();
+    long long k = 3;
+    int len = getLongestSubarray(a,n, k);
+    cout << "The length of the longest subarray is: " << len << "\n";
+    return 0;
+}
+
