0b03a3a26db1aac3d700d48df24a58f8ea1deddd
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-11-08 12:23:04 +0600

    next permutation bruteforce

--- step3-Solving Problems on Arrays/3.2 -Medium/14_Varitey2 of previous.cpp
+++ step3-Solving Problems on Arrays/3.2 -Medium/14_Varitey2 of previous.cpp
@@ -22,75 +22,82 @@ To maintain relative ordering, 1 must occur before 2, and -4 must occur before -
 Leftover positive elements are 3 and 4 which are then placed at the end of the array.
 */
 
+/************
+  Note: In case any image/dry run is not clear please refer to the video attached at the bottom.
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> reArrangeBySign(vector<int>&A, int n)
-{
-     // Define 2 vectors, one for storing positive 
-    // and other for negative elements of the array.
-    vector<int>pos;
-    vector<int>neg;
-
-    // Segregate the array into positives and negatives.
-    for(int i=0; i<n; i++)
-    {
-        if(A[i]>0) pos.push_back(A[i]);
-        else neg.push_back(A[i]);
+  vector<int> RearrangebySign(vector<int>A, int n){
+    
+  // Define 2 vectors, one for storing positive 
+  // and other for negative elements of the array.
+  vector<int> pos;
+  vector<int> neg;
+  
+  // Segregate the array into positives and negatives.
+  for(int i=0;i<n;i++){
+      
+      if(A[i]>0) pos.push_back(A[i]);
+      else neg.push_back(A[i]);
+  }
+  
+  // If positives are lesser than the negatives.
+  if(pos.size() < neg.size()){
+      
+    // First, fill array alternatively till the point 
+    // where positives and negatives ar equal in number.
+    for(int i=0;i<pos.size();i++){
+      
+      A[2*i] = pos[i];
+      A[2*i+1] = neg[i];
     }
-
-    //if pos are leasser then negatives.
-    if(pos.size() < neg.size())
-    {
-        for(int i=0; i<n; i++)
-        {
-            A[2*i] = pos[i];
-            A[2*i+1] = neg[i];
-        }
+    
+    // Fill the remaining negatives at the end of the array.
+    int index = pos.size()*2;
+    for(int i = pos.size();i<neg.size();i++){
         
-        //fill the remaining elements at the end of the arr
-
-        int index = pos.size()*2;
-
-        for(int i= pos.size(); i<neg.size(); i++)
-        {
-            A[index]= neg[i];
-            index++;
-        }
+        A[index] = neg[i];
+        index++;
     }
-    //If negatives are lesser then the positive
-    else
-    {
-        for(int i=0; i<neg.size(); i++)
-        {
-            A[2*i] = pos[i];
-            A[2*i+1] = neg[i];
-        }
-
-        //fill the reaming positive at the end of the array.
-
-        int index = neg.size()*2;
-        for(int i=neg.size();  i< pos.size(); i++)
-        {
-            A[index] = pos[i];
-            index++;
-        }
-
-        return A;
+  }
+  
+  // If negatives are lesser than the positives.
+  else{
+      
+      // First, fill array alternatively till the point 
+      // where positives and negatives ar equal in number.
+      for(int i=0;i<neg.size();i++){
+      
+      A[2*i] = pos[i];
+      A[2*i+1] = neg[i];
+  }
+    
+    // Fill the remaining positives at the end of the array.
+    int index = neg.size()*2;
+    for(int i = neg.size();i<pos.size();i++){
+        
+        A[index] = pos[i];
+        index++;
     }
-
+  }
+  return A;
+    
 }
 
-int main()
-{
-    int n=6;
-    vector<int> A {1,2,-4,-5,3,4};
+int main() {
+    
+  // Array Initialisation.
+  int n = 6;
+  vector<int> A {1,2,-4,-5,3,4};
 
-    vector<int> ans= reArrangeBySign(A, n);
+  vector<int> ans = RearrangebySign(A,n);
+  
+  for (int i = 0; i < ans.size(); i++) {
+    cout << ans[i] << " ";
+  }
 
-    for(int i=0; i< ans.size(); i++)
-    {
-        cout<< ans[i] << " ";
-    }
-    return 0;
+  return 0;
+}
 }
